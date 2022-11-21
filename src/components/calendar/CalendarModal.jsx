9ada9899b29d12ae4d154bc039e6a7cd51d91865
garyDav <gary.2810.dav@gmail.com>
2022-11-21 11:53:42 -0400

    view: modal

---
+++ src/components/calendar/CalendarModal.jsx
@@ -0,0 +1,6 @@
+
+export const CalendarModal = () => {
+  return (
+    <div>Hola Mundo</div>
+  )
+}
--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -3,6 +3,9 @@ import moment from 'moment'
 
 import { Navbar } from '../ui/Navbar'
 import { messages } from '../../helpers/calendar-messages-es'
+import { CalendarEvent } from './CalendarEvent'
+import { useState } from 'react'
+import { CalendarModal } from './CalendarModal'
 
 import 'moment/dist/locale/es-mx'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
@@ -16,10 +19,41 @@ const myEventsList = [
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
     bgcolor: '#fafafa',
+    notes: 'Comprar el pastel',
+    user: {
+      _id: '123',
+      name: 'Gary'
+    }
   },
 ]
 
 export const CalendarScreen = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month')
+
+  const onDoubleClick = (e) => {
+    console.log('Joder', e)
+  }
+  const onSelectEvent = (e) => {
+    console.log(e)
+  }
+  const onViewChange = (e) => {
+    setLastView(e)
+    localStorage.setItem('lastView', e)
+  }
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white',
+    }
+
+    return {style}
+  }
+
   return (
     <div className='calendar-screen'>
       <Navbar />
@@ -30,7 +64,17 @@ export const CalendarScreen = () => {
         startAccessor='start'
         endAccessor='end'
         messages={messages}
+        eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view={lastView}
+        components={{
+          event: CalendarEvent
+        }}
       />
+
+      <CalendarModal />
     </div>
   )
 }
